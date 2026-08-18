b7b34f11de905cbc3595fd1b442d46d34a742896
saikyoupurochita-dot <saikyoupurochita@gmail.com>
2026-08-18 15:31:26 +0900

    Add files via upload

--- app/src/main/java/com/retrivedmods/wclient/game/module/combat/AntiCrystalModule.kt
+++ app/src/main/java/com/retrivedmods/wclient/game/module/combat/AntiCrystalModule.kt
@@ -4,20 +4,31 @@ package com.retrivedmods.wclient.game.module.combat
 import com.retrivedmods.wclient.game.InterceptablePacket
 import com.retrivedmods.wclient.game.Module
 import com.retrivedmods.wclient.game.ModuleCategory
+import org.cloudburstmc.math.vector.Vector3f
+import org.cloudburstmc.protocol.bedrock.packet.MovePlayerPacket
 import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
 
 class AntiCrystalModule : Module("anti_crystal", ModuleCategory.Combat) {
 
-    private var ylevel by floatValue("ylevel", 0.4f, 0.1f..1.61f)
+    private var reduce by floatValue("reduce", 0.6f, 0.1f..1f)
 
     override fun beforePacketBound(interceptablePacket: InterceptablePacket) {
         if (!isEnabled) {
             return
         }
 
+        val actorPos = session.localPlayer.vec3Position
+        val newY = actorPos.y - reduce
+
         val packet = interceptablePacket.packet
-        if (packet is PlayerAuthInputPacket) {
-            packet.position.add(0.0, -ylevel.toDouble(), 0.0)
+        when (packet) {
+            is PlayerAuthInputPacket -> {
+                packet.position = Vector3f.from(packet.position.x, newY, packet.position.z)
+            }
+
+            is MovePlayerPacket -> {
+                packet.position = Vector3f.from(packet.position.x, newY, packet.position.z)
+            }
         }
     }
 
